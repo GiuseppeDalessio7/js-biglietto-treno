f9bd6d5c21b97043eaee2ff28c514d5d3e4c6095
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-08-23 16:31:38 +0200

    Push for curiosity

--- js/myscript.js
+++ js/myscript.js
@@ -6,9 +6,43 @@ va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va stampato in forma umana (ovvero con massimo due decimali, per indicare i centesimi sul prezzo)*/
 
 //il numero di chilometri che vuole percorrere
-let km = prompt("Quanti Chilometri devi percorrere?");
+const km = Number(prompt('Quanti Chilometri devi percorrere?'));
 console.log ('I chilometri da percorrere sono:'+ km);
-//l'età del passeggero
-let age = prompt ("Quale è la tua età?") 
-console.log("La tua età è:" + age);
+if (km != (Number)){
+alert("Ops, ripeti la procedura inserendo per favore i km che percorrerai");
+}
 
+//l'età del passeggero
+const age = Number(prompt("Quale è la tua età?")); 
+console.log("La tua età è:" + age);
+if (age != (Number)){
+    alert("Ops, ripeti la procedura inserendo per favore la tua età ");
+}
+//il prezzo del biglietto è definito in base ai km (0.21 € al km)
+const ticketPrice = km * 0.21;
+console.log("Il costo del biglietto è:€" + ticketPrice)
+//stampa il prezzo del biglietto
+
+
+let total 
+//va applicato uno sconto del 20% per i minorenni
+if(age <= 18 ) {
+    let = discount20 = ticketPrice * (20 % 100);
+    total = discount20 - ticketPrice;
+}
+//va applicato uno sconto del 40% per gli over 65.
+else if (age >= 65) {
+    let = discount65 = ticketPrice * (40 % 100);
+    total = discount65 - ticketPrice;
+}
+
+else {
+    total = ticketPrice
+}
+
+ let finalPrice = total.toFixed(2)
+ console.log(total);
+ console.log(finalPrice);
+
+// //stampo su documento 
+ document.getElementById("ticketPrice").innerHTML = "Il costo del biglietto è:€" + finalPrice;
