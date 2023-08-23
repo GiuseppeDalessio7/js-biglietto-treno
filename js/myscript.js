24950218573bdc8db6b89f2ec7074b81dc7e7926
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-08-23 15:11:07 +0200

    Initial commit
    Add 2 km and age

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+/*Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va stampato in forma umana (ovvero con massimo due decimali, per indicare i centesimi sul prezzo)*/
+
+//il numero di chilometri che vuole percorrere
+let km = prompt("Quanti Chilometri devi percorrere?");
+console.log ('I chilometri da percorrere sono:'+ km);
+//l'età del passeggero
+let age = prompt ("Quale è la tua età?") 
+console.log("La tua età è:" + age);
+
